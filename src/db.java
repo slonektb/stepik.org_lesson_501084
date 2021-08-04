52ff4e52769d0289dd54e6764cc160c9bd48318b
slonektb <gb_alex@mail.ru>
2021-08-04 22:05:13 +0500

    step 5

--- src/db.java
+++ src/db.java
@@ -5,9 +5,10 @@ public class db {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         conn.Conn();
         conn.CreateDB();
-        conn.insert_type("Абиссинская кошка");
-        conn.insert_type("Австралийский мист");
-        conn.insert_type("Американская жесткошерстная");
+//        conn.insert_type("Абиссинская кошка");
+//        conn.insert_type("Австралийский мист");
+//        conn.insert_type("Американская жесткошерстная");
+        conn.add_all_types();
         conn.CloseDB();
     }
 }
