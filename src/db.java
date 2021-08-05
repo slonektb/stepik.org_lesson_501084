699969c73a9e06e86aaa3307545197e97e1c53c0
slonektb <gb_alex@mail.ru>
2021-08-05 12:50:11 +0500

    step 10

--- src/db.java
+++ src/db.java
@@ -15,9 +15,10 @@ public class db {
 //        conn.get_type_where("id < 15");
 //        conn.get_type_where("type LIKE '%а'");
 //        conn.get_all_types();
-        conn.insert_cat("Вася","Дворянин",1,0.5); //type_id = 71
-        conn.insert_cat("Мурло","Русская голубая кошка",5,5.0); //type_id = 48
-        conn.insert_cat("Кокс","Черный демон",3,4.5);
+//        conn.insert_cat("Вася","Дворянин",1,0.5); //type_id = 71
+//        conn.insert_cat("Мурло","Русская голубая кошка",5,5.0); //type_id = 48
+//        conn.insert_cat("Кокс","Черный демон",3,4.5);
+        conn.add_more_cats(5000);
 
         conn.CloseDB();
     }
